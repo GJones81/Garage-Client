cead8fae45d6d2fbd640f314c9ca3838c3d49f78
gjones81 <jones.guy81@gmail.com>
2020-05-20 12:36:28 -0700

    Updating

--- src/content/pages/Discovery.js
+++ src/content/pages/Discovery.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Discovery = props => {
 
     return (
-        <div>This page is to </div>
+        <div>
+        	<h3>Discover Garage Sales In Your Area</h3>
+        </div>
     )
 }
 
