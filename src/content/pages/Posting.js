9926e7a0aa92991cc528eabe36c84aca82a9027d
gjones81 <jones.guy81@gmail.com>
2020-05-20 11:27:17 -0700

    Updated Navbar to include Posting and Discovery

--- src/content/pages/Posting.js
+++ src/content/pages/Posting.js
@@ -12,9 +12,11 @@ const Posting = props => {
         //As a suggestion maybe we should have two different forms and use routers??
         <div>
             <div className="addForms">
+                <h3>New Items</h3>
                 <Route exact path='/newItem' component={newItem} /> 
             </div>
             <div>
+                <h3>New List</h3>
                 <Route exact path='/newListing' component={newList} />
                 
             </div>
