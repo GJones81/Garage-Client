ea70e608c3f6b1f17b30a3f2f63389dd47e8bc40
gjones81 <jones.guy81@gmail.com>
2020-05-23 19:54:17 -0700

    Added modals for Creating and Editing a sale

--- src/content/pages/New/EditSale.js
+++ src/content/pages/New/EditSale.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap'
 
 
 //Need to add this as a Router i think on Profile.js and import it??
@@ -15,6 +16,24 @@ const EditSale = props => {
     let [ address, setAddress ] = useState(props.sale.address)
     let [ date, setDate ] = useState(props.sale.date)
     let [ list, setList ] = useState(initialId)
+
+    //Reactstrap states
+    const [modal, setModal] = useState(false);
+    const [nestedModal, setNestedModal] = useState(false);
+    const [closeAll, setCloseAll] = useState(false)
+
+    //Reactstrap functions
+    const toggle = () => setModal(!modal);
+
+    const toggleNested = () => {
+      setNestedModal(!nestedModal);
+      setCloseAll(false);
+    }
+
+    const toggleAll = () => {
+      setNestedModal(!nestedModal);
+      setCloseAll(true);
+    }
     
     //am i targeting this right, because sale
     const submitEdit = e => {
@@ -56,15 +75,34 @@ const EditSale = props => {
 
     return (
         <div>
-            <form onSubmit={submitEdit}>
-                <label>Date</label>
-                
-                <input type='text' name='date' onChange={e => setDate(e.target.value)} /> 
-                <label>Address</label>
-                <input type='text' name='address' onChange={e => setAddress(e.target.value)} /> 
-                <select onChange={handleChange}> {listz} </select>
-                <button type='submit'>submit</button>
-            </form>
+            <Button color="danger" onClick={toggle}>Edit This Sale</Button>
+            <Modal isOpen={modal} toggle={toggle}>
+                <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+                <ModalBody>
+                    <form onSubmit={submitEdit}>
+                        <label>Date</label>
+                        <input type='text' name='date' onChange={e => setDate(e.target.value)} /> 
+                        <label>Address</label>
+                        <input type='text' name='address' onChange={e => setAddress(e.target.value)} /> 
+                        <select onChange={handleChange}> {listz} </select>
+                        <button type='submit'>submit</button>
+                    </form>
+                    <br />
+                    <Button color="success" onClick={toggleNested}>Show Nested Modal</Button>
+                    <Modal isOpen={nestedModal} toggle={toggleNested} onClosed={closeAll ? toggle : undefined}>
+                        <ModalHeader>Nested Modal title</ModalHeader>
+                        <ModalBody>Stuff and things</ModalBody>
+                        <ModalFooter>
+                            <Button color="primary" onClick={toggleNested}>Done</Button>{' '}
+                            <Button color="secondary" onClick={toggleAll}>All Done</Button>
+                        </ModalFooter>
+                    </Modal>
+                </ModalBody>
+                <ModalFooter>
+                    <Button color="primary" onClick={toggle}>Do Something</Button>{' '}
+                    <Button color="secondary" onClick={toggle}>Cancel</Button>
+                </ModalFooter>
+            </Modal>
         </div>
     )
 }
