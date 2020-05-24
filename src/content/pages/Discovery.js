63368350adb7da088e9df3ee0958f00c24c6b0ac
gjones81 <jones.guy81@gmail.com>
2020-05-24 12:55:55 -0700

    Displaying each Sales listed items on Discovery page

--- src/content/pages/Discovery.js
+++ src/content/pages/Discovery.js
@@ -4,15 +4,32 @@ import styled from 'styled-components'
 import MapboxGLMap from './MapboxGLMap'
 
 const Discovery = props => {
-  console.log(props.discoveries)
+
+  
+
   let discovery = props.discoveries.publicSales.map((d, i) =>{
-    return (
-      <div key={i}>
-        <p>Address: {d.address}</p>
-        <p>Date: {d.date}</p>
-      </div>
+    console.log(props.discoveries)
+    let itemz = d.list.item.map((x, y) => {
+        return (
+        <div key={y}>
+            <img src={x.image}></img>
+            <p>Item</p>
+            <p>{x.name}</p>
+            <p>Price</p>
+            <p>${x.price}</p>
+            <p>Condition</p>
+            <p>{x.condition}</p>
+        </div> 
+        )
+    })
+    return(
+          <div key={i}>
+          <p>Address: {d.address}</p>
+          <p>Date: {d.date}</p>
+            {itemz}
+        </div>
     )
-  })
+})
 
       let Header = styled("header")`
       width: 100vw;
